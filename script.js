448b5bf761613f3c287e7786cef0e5c2015bf375
justincbal <justinseebee@hotmail.com>
2022-09-24 19:37:00 -0400

    fix missing button

--- script.js
+++ script.js
@@ -1,7 +1,12 @@
 const buttons = document.querySelectorAll('.btn');
+const output = document.querySelector('.output');
+let num1;
 
 buttons.forEach(btn => {
     btn.addEventListener('click', e => {
-        console.log(e.target.textContent);
+        num1 = e.target.textContent;
+        console.log(num1);
+        console.log(typeof num1);
+        output.textContent = num1;
     })
 })
